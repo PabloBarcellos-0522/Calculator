1818e35e26e0868b31eb7d3e9bf2c7a674a0f8fe
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-04-27 21:31:12 -0300

    all simple calculations

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   let num1 = 0
   let num2 = 0
   let result = null
+  let choice = ""
 
   var buttonNumbers = document.getElementsByClassName("num")
+  var zero = document.getElementById("zero")
+  var dot = document.getElementById("dot")
   var multiplicacao = document.getElementById("multiplicacao")
   var adicao = document.getElementById("adicao")
   var subitracao = document.getElementById("subitracao")
@@ -13,66 +16,134 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < buttonNumbers.length; i++) {
     buttonNumbers[i].addEventListener("click", function () {
       writing = writing + buttonNumbers[i].getAttribute("data-num")
-      num1 = writing
+      if (choice == "") {
+        num1 = writing
+      }
       document.getElementById("result").innerHTML = writing
     })
   }
+  zero.addEventListener("click", function () {
+    writing = writing + "0"
+    if (choice == "") {
+      num1 = writing
+    }
+    document.getElementById("result").innerHTML = writing
+  })
+  dot.addEventListener("click", function () {
+    console.log(writing)
+    writing = writing + "."
+    if (choice == "") {
+      num1 = writing
+    }
+    document.getElementById("result").innerHTML = writing
+  })
 
   multiplicacao.addEventListener("click", function () {
-    // if (result == 0) {
-    //   if (num1 == 0) {
-    //     num1 = writing
-    //   } else if (num2 == 0) {
-    //     num2 = writing
-    //   } else {
-    //     result = num1 * num2
-    //     num1 = 0
-    //     num2 = 0
-    //   }
-    // } else {
-    //   if (num1 == 0) {
-    //     num1 = writing
-    //   } else {
-    //     result *= num1
-    //     num1 = 0
-    //   }
-    // }
-    if (result == null) {
-      result = 1
+    if (choice != "" && choice != "M") {
+      equal()
     }
-    result *= writing
-
-    console.log(num1, num2, result)
-
-    writing = ""
+    if (choice == "") {
+      num1 = writing
+      writing = ""
+      console.log(num1, num2, result)
+      choice = "M"
+    } else {
+      num2 = writing
+      if (result == null) {
+        result = num1 * num2
+      } else {
+        result *= num2
+      }
+      writing = result
+      document.getElementById("result").innerHTML = result
+      writing = ""
+      console.log(num1, num2, result)
+    }
   })
 
   adicao.addEventListener("click", function () {
-    clear()
+    if (choice != "" && choice != "A") {
+      equal()
+    }
+    if (choice == "") {
+      num1 = writing - 0
+      writing = ""
+      console.log(num1, num2, result)
+      choice = "A"
+    } else {
+      num2 = writing - 0
+      if (result == null) {
+        result = num1 + num2
+      } else {
+        result += num2
+      }
+      writing = result
+      document.getElementById("result").innerHTML = result
+      writing = ""
+      console.log(num1, num2, result)
+    }
   })
 
   subitracao.addEventListener("click", function () {
-    clear()
+    if (choice != "" && choice != "S") {
+      equal()
+    }
+    if (choice == "") {
+      num1 = writing - 0
+      writing = ""
+      console.log(num1, num2, result)
+      choice = "S"
+    } else {
+      num2 = writing - 0
+      if (result == null) {
+        result = num1 - num2
+      } else {
+        result -= num2
+      }
+      writing = result
+      document.getElementById("result").innerHTML = result
+      writing = ""
+      console.log(num1, num2, result)
+    }
   })
 
-  equals.addEventListener("click", function () {
-    result *= num1
-    clear()
-
-    writing = ""
-    console.log(result, writing)
-    document.getElementById("result").innerHTML = result
-  })
+  equals.addEventListener("click", equal)
 
   function clear() {
     writing = ""
     document.getElementById("result").innerHTML = 0
   }
-})
 
-// if (num1 == 0) {
-//   num1 = writing
-// } else if (num2 == 0) {
-//   num1 = writing
-// } else if (num1 != 0 && num2 != 0) {
-// }
+  function equal() {
+    num2 = writing - 0
+    console.log(num1, num2, result)
+    if (choice == "M") {
+      if (result == null) {
+        result = num1 * num2
+      } else {
+        result *= num2
+      }
+    }
+
+    if (choice == "A") {
+      if (result == null) {
+        result = num1 + num2
+      } else {
+        result += num2
+      }
+    }
+
+    if (choice == "S") {
+      if (result == null) {
+        result = num1 - num2
+      } else {
+        result -= num2
+      }
+    }
+
+    writing = result
+    document.getElementById("result").innerHTML = result
+    result = null
+    choice = ""
+  }
+})
